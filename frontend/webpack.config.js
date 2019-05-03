c9794acaf022413e16198f5a2d2f03fb854f65d4
ppworks <koshikawa@ppworks.jp>
2019-05-03 10:22:51 +0900

    manifest.json

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,14 +1,20 @@
 const path = require('path');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
   mode: 'development',
   // エントリーポイントの設定
-  entry: './src/js/app.js',
+  entry: {
+    app: './src/js/app.js',
+  },
   // 出力の設定
   output: {
     // 出力するファイル名
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     // 出力先のパス（絶対パスを指定する必要がある）
     path: path.join(__dirname, '../public/js')
-  }
+  },
+  plugins: [
+    new ManifestPlugin()
+  ],
 };
