31252743acc6ad2bd1b4d1b755b7173602a19e87
ppworks <koshikawa@ppworks.jp>
2019-05-06 11:23:51 +0900

    Tweak posts table

--- database/migrations/20190505130505_create_posts.sql
+++ database/migrations/20190505130505_create_posts.sql
@@ -2,6 +2,7 @@
 -- SQL in this section is executed when the migration is applied.
 CREATE TABLE posts (
     id text NOT NULL PRIMARY KEY,
+    incremental_key SERIAL UNIQUE,
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
     author text NOT NULL,
